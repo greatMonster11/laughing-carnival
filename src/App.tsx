38f1c65d6a414f858ac6882609caaccf4b0658ff
greatMonster11 <nguyenphuocthanhtn2015@gmail.com>
2020-08-10 14:57:59 +0700

    feat(deploy): complete simple chat and deploy it on firebase

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { useState, useEffect } from "react";
 
-import Button from "@material-ui/core/Button";
-import { FormControl, Input, InputLabel } from "@material-ui/core";
+import { FormControl, Input } from "@material-ui/core";
 
 import Message from "./components/Message";
 import { IMessage } from "./components/Message";
 import db from "./firebase";
+import firebase from "firebase";
+import FlipMove from "react-flip-move";
+
+import { IconButton } from "@material-ui/core";
+import SendIcon from "@material-ui/icons/Send";
 
 import "./App.css";
 
@@ -16,9 +20,13 @@ function App() {
 
   useEffect(() => {
     // run once when the app loads components
-    db.collection("messages").onSnapshot((snapshot) => {
-      setMessages(snapshot.docs.map((doc) => doc.data()));
-    });
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        setMessages(
+          snapshot.docs.map((doc) => ({ id: doc.id, message: doc.data() }))
+        );
+      });
   }, []);
 
   useEffect((): void => {
@@ -28,36 +36,47 @@ function App() {
 
   const sendMessage = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    setMessages([...messages, { message: input, username }]);
-    setInput("");
+    db.collection("messages").add({
+      message: input,
+      username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+    setInput(""); // also need to clean, for more UX
   };
 
   return (
     <div className="App">
+      <img
+        src="https://facebookbrand.com/wp-content/uploads/2019/10/Messenger_Logo_Color_RGB.png?w=100&h=100"
+        alt="Messenger logo"
+      />
       <h1>Hello Awesome !!! </h1>
       <h1>Welcome {username}</h1>
-      <form onSubmit={sendMessage}>
-        <FormControl>
-          <InputLabel>Enter a message ...</InputLabel>
+      <form className="app__form" onSubmit={sendMessage}>
+        <FormControl className="app__formControl">
           <Input
+            className="app__input"
+            placeholder="Enter a message ..."
             value={input}
             onChange={(event: React.ChangeEvent<HTMLInputElement>): void =>
               setInput(event.target.value)
             }
           />
-          <Button
+          <IconButton
+            className="app__iconButton"
             disabled={!input}
-            variant="contained"
             color="primary"
             type="submit"
           >
-            Send Message
-          </Button>
+            <SendIcon />
+          </IconButton>
         </FormControl>
       </form>
-      {messages.map((message: IMessage) => (
-        <Message message={message} username={username} />
-      ))}
+      <FlipMove>
+        {messages.map(({ id, message }: { id: string; message: IMessage }) => (
+          <Message key={id} message={message} username={username} />
+        ))}
+      </FlipMove>
     </div>
   );
 }
--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
 import { Card, CardContent, Typography } from "@material-ui/core";
 
@@ -7,27 +7,28 @@ import "./Message.css";
 export interface IMessage {
   message: string;
   username?: string;
+  timestamp?: string;
 }
 
-function Message({
-  message,
-  username,
-}: {
-  message: IMessage;
-  username: string;
-}) {
-  const isUser = username === message.username;
-  return (
-    <div className={`message ${isUser && "message__user"}`}>
-      <Card className={isUser ? "message__userCard" : "message__guestCard"}>
-        <CardContent>
-          <Typography>
-            {message.username}: {message.message}
-          </Typography>
-        </CardContent>
-      </Card>
-    </div>
-  );
-}
+const Message = forwardRef(
+  (
+    { message, username }: { message: IMessage; username: string },
+    ref: any
+  ) => {
+    const isUser = username === message.username;
+    return (
+      <div ref={ref} className={`message ${isUser && "message__user"}`}>
+        <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+          <CardContent>
+            <Typography>
+              {!isUser && `${message.username || "Unknow User"}: `}{" "}
+              {message.message}
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
+);
 
 export default Message;
